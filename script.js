998bbdbe3c5c1780701af7f4749567bf4ee254ca
Kunal-Banerjee <160137385+Kunal-Banerjee@users.noreply.github.com>
2024-09-28 19:20:06 +0530

    added index , javascript and css file

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+document.getElementById("myList").style.textAlign = "center";
+function openPDF() {
+    window.location.href = 'kunal_CV.pdf';
+}
